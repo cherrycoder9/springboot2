a6364a912b8671d8ede4cc592ce0be8fc4863a7f
cherrycoder9 <cherrycoder9@gmail.com>
2024-07-24 18:39:56 +0900

    no message

--- src/main/resources/static/js/board/allpage.js
+++ src/main/resources/static/js/board/allpage.js
@@ -2,39 +2,46 @@ doBoard();
 function doBoard() {
     console.log('doBoard');
 
+    // 게시물 목록 조회
     $.ajax({
+        /*
+        public class BoardDto {
+            private long bno ;          // 번호
+            private String btitle;      // 제목
+            private String bcontent;        // 내용
+            private String bfile;       // 첨부파일
+            private long bview ;        // 조회수
+            private String bdate;       // 작성일
+            // 카테고리
+            private long bcno ;         // 카테고리 번호
+            private String bcname ;         // 카테고리 이름
+            // 회원
+            private long no ;          // 작성자 번호
+            private String id;          // 작성자 아이디
+        }
+        */
         method: 'get',
         url: '/board/all',
         contentType: "application/json",
 
         success: result => {
+            // 글 목록 배열을 반환받으면 출력 
             console.log(result);
-            if (result) {
-                //어디에
-                let boardBox = document.querySelector('#boardBox');
 
-                //무엇을
-                let html = '';
-                html += `<div> ${result.bno} </div> `;
-                html += `<div> ${result.btitle} </div> `;
-                html += `<div> ${result.no} </div> `;
-                html += `<div> ${result.bdate} </div> `;
-                html += `<div> ${result.bview} </div> `;
-                console.log(result.bno);
-                console.log(result.btitle);
-                console.log(result.no);
-                console.log(result.bdate);
-                console.log(result.bview);
+            //어디에
+            let boardBox = document.querySelector('#boardBox');
+            let html = '';
 
-                //게시물번호, 제목,작성자(id),작성일,조회수
-                boardBox.innerHTML = html;
-                console.log(boardBox);
-            } else {
-                console.log('test1');
-                alert('잘못된 방식입니다.');
+            // 출력사항: 게시물 번호, 제목, 작성자(id), 작성일, 조회수
+            for (let i = 0; i < result.length; i++) {
+                html += `<div> ${result[i].bno} </div>`;
+                html += `<div> ${result[i].btitle} </div>`;
+                html += `<div> ${result[i].id} </div>`;
+                html += `<div> ${result[i].bdate} </div>`;
+                html += `<div> ${result[i].bview} </div>`;
             }
-            console.log('test2');
-
+            boardBox.innerHTML = html;
+            console.log('test4');
         }//success end
     });//ajax end
 } //doBoard end
