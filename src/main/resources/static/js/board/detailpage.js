b597b8d01cc631e3f70d4db1e31ca6afd954c656
cherrycoder9 <cherrycoder9@gmail.com>
2024-07-25 10:50:48 +0900

    no message

---
+++ src/main/resources/static/js/board/detailpage.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    loadBoardDetail();
+});
+
+function loadBoardDetail() {
+    console.log('loadBoardDetail()');
+    // URL에서 bno 파라미터 가져오기
+    const urlParams = new URLSearchParams(window.location.search);
+    const bno = urlParams.get('bno');
+
+    // 게시물 상세 정보 가져오기
+    $.ajax({
+        method: 'GET',
+        url: `/board/detail?bno=${bno}`,
+        contentType: "application/json",
+        success: (data) => {
+            displayBoardDetail(data);
+        }
+    });
+}
+
+function displayBoardDetail(board) {
+    const boardDetail = document.querySelector('#boardDetail');
+    boardDetail.innerHTML = `
+        <h2>${board.btitle}</h2>
+        <p>카테고리: ${board.bcname}</p>
+        <p>작성자: ${board.id}</p>
+        <p>작성일: ${board.bdate}</p>
+        <p>조회수: ${board.bview}</p>
+        <div>
+            <p>${board.bcontent}</p>
+        </div>
+    `;
+}
+
+function editPost() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const bno = urlParams.get('bno');
+    window.location.href = `/board/update?bno=${bno}`;
+}
+
+function deletePost() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const bno = urlParams.get('bno');
+
+    if (confirm("정말로 삭제하시겠습니까?")) {
+        $.ajax({
+            method: 'DELETE',
+            url: `/board/delete`,
+            data: JSON.stringify({ bno }),
+            contentType: "application/json",
+            success: (result) => {
+                if (result) {
+                    alert('게시물이 삭제되었습니다.');
+                    window.location.href = '/board/all';
+                } else {
+                    alert('삭제에 실패했습니다.');
+                }
+            }
+        });
+    }
+}
