51311e11ee11ced610fca456a2100d32b576b73a
cherrycoder9 <cherrycoder9@gmail.com>
2024-07-25 16:11:59 +0900

    no message

--- src/main/java/web/service/FileService.java
+++ src/main/java/web/service/FileService.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class FileService {
@@ -14,7 +15,16 @@ public class FileService {
     // 1. 파일 업로드
     public String fileUpload(final MultipartFile multipartFile) throws IOException {
         // 첨부 파일의 실제 파일 이름 추출
-        final String fileName = multipartFile.getOriginalFilename();
+        // 클라이언트(유저)들이 서로 다른 파일내용의 같은 파일명으로 업로드 했을때 식별이 불가능
+        // 해결방안 : 1. UUID, 2. 조합식별설계(주로 업로드 날짜/시간)
+        final String uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
+        String fileName = multipartFile.getOriginalFilename();
+        // UUID + 파일명 합치기, uuid와 파일명 구분하는 문자 조합, 파일명의 _(언더바)가 존재하면 안된다. 
+        // 추후에 _(언더바) 기준으로 분해하면 [0]UUID, [1]순수파일명임 
+        // "문자열".replaceAll("기존문자", "새로운문자"): 만약에 문자열내 기존문자가 존재하면 새로운 문자로 치환해서 반환
+        fileName = uuid + "_" + (fileName != null ? fileName.replaceAll("_", "-") : null); // 파일명에 '_'문자가 존재하면 '-'문자로 변경.
+        // 왜냐하면, uuid랑 파일명이랑 구분하기 위해서
         System.out.println("fileName = " + fileName);
         /*// 저장할 경로 만들기
         C:\Users\codemaster\Documents\workspace\springboot2\src\main\resources\static\ upload*/
