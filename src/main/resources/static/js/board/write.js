fd58232a15d2c2af3996e696ba285e04a06252fb
tj-bu-703-023 <none@naver.com>
2024-07-25 09:35:31 +0900

    2024-07-24

---
+++ src/main/resources/static/js/board/write.js
@@ -0,0 +1,9 @@
+console.log('write.js');
+
+function write(){ console.log('write()');
+    let category = document.querySelector('#selectbox');
+    let btitle = document.querySelector('#btitle');
+    let bcontent = document.querySelector('#bcontent');
+
+
+}
